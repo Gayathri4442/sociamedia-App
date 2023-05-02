8993c671596dcba839368b99c5cbc80c83de508f
Gayathri4442 <gayumuvi515@gmail.com>
2023-05-02 12:02:23 +0530

    media

--- client/src/Components/Post/Post.jsx
+++ client/src/Components/Post/Post.jsx
@@ -11,8 +11,8 @@ import { likePost } from "../../api/PostsRequests";
 const Post = ({data}) => {
 
 const {user} = useSelector((state)=>state.authReducer.authData)
-const [liked, setLiked] = useState(data.likes.includes(user._id));
-const [likes, setLikes] = useState(data.likes.length)
+const [liked, setLiked] = useState(data.likes?.includes(user._id));
+const [likes, setLikes] = useState(data.likes?.length)
 
 const handleLike = () => {
   likePost(data._id, user._id);
